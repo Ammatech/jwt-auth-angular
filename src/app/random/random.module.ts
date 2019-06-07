7b38eb3ed8e0a448a359b90dcdad8c1f40616075
Tony Sykes <tony.sykes@ammatech.com.au>
2019-06-07 17:53:54 +1000

    Upgrade to v8

--- src/app/random/random.module.ts
+++ src/app/random/random.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RandomNumberComponent } from './containers/random-number/random-number.component';
 import { RandomRoutingModule } from './random.routing.module';
-import { MatButtonModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [RandomNumberComponent],
